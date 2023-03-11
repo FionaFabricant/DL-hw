ef3cceeb04d2c06b78f8a60cbf21fa12d3861ef1
rsdadbewhucxz <1079987736@qq.com>
2023-03-11 16:00:04 +0800

    Update Counter.sol

--- contracts/learnblockchain homework/w1-1/Counter.sol
+++ contracts/learnblockchain homework/w1-1/Counter.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Counter{
-    uint public num=100;
+//interface
+interface ICounter{
+    function count() external view returns(uint);
+    function inc() external;
+    function dec() external;
+}
 
-    function add(uint x) external view returns(uint result){
-        result = num+x;
+contract CallInterface{
+    uint public count;
+    function examples(address _counter) external{
+        ICounter(_counter).inc();
+        ICounter(_counter).count();
+
     }
 }
